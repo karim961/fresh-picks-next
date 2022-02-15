32a0aec192c3631471491d4c8bef2156f0d11238
Karim Abbas <32097461+karim961@users.noreply.github.com>
2022-02-15 19:17:00 +0200

    feat: add get in touch section feat: add footer chore: modify mobile styles

--- config/menu.ts
+++ config/menu.ts
@@ -1,7 +1,11 @@
+import ROUTES from './routes';
+
 export const MENU = [
-  { label: 'test', url: '/' },
+  { label: 'Home', url: ROUTES.INDEX },
+  { label: 'About Us', url: ROUTES.ABOUT },
+
   {
-    label: 'test',
+    label: 'Products',
     items: [
       { label: 'test1', url: 'https://www.google.com.lb/' },
       { label: 'test1', url: 'https://www.google.com.lb/' },
